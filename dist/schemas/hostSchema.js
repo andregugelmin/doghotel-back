1015ba8bc2856d7d334f8b9dafd606c3c3734253
André Heiden Gugelmin <andregugelmin1996@gmail.com>
2022-08-15 10:53:03 -0300

    Delete dist directory

--- dist/schemas/hostSchema.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-export var createHostSchema = Joi.object({
-    price: Joi.number().required(),
-    minWeight: Joi.number().required(),
-    maxWeight: Joi.number().required(),
-    city: Joi.string().required(),
-    address: Joi.string().required()
-});
