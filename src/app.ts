e7e947e060cff8f68d2b16ed594fc04b0f95ea89
andregugelmin <andregugelmin1996@gmail.com>
2022-08-15 10:47:18 -0300

    app.ts adjustments

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import cors from 'cors';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/errorHandler.js';
 import router from './routers/index.js';
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => res.send('Application is online'));
+
 app.use(router);
 app.use(errorHandler);
 export default app;
